ab315be6df2f145b36cc2589754662a8037b4953
Anastasia <63377276+Perrrsik6150@users.noreply.github.com>
2021-11-15 16:19:00 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Window1 = new Window1(); //create your new form.
+            Window1.Show(); //show the new form.
+            this.Close();
+        }
+
+        public class bio
+        {
+            public string Title { get; set; }
+            public string Company { get; set; }
+            public int Price { get; set; }
+        }
+            
+    }
+}
